4dbd572c54607feffa7e95f09b54c159df89095b
Ibsa Abraham <ibsaabraham663@gmail.com>
2022-04-23 21:46:45 +0300

    Feat: crud operation on group task
    edit/remove/mark

--- TaskManager/projects/urls.py
+++ TaskManager/projects/urls.py
@@ -19,10 +19,13 @@ urlpatterns = [
     path('group-project/', views.create_group_project, name = "add-group-project"),
     path('delete-pp/<str:pk>', views.delete_personalProject, name = "delete-pp"),
     path('delete-pt/<str:pk>', views.delete_personalTask, name = "delete-pt"),
+    path('delete-grouptask/<str:pk>', views.delete_group_task, name = "delete-grouptask"),
+    path('update-grouptask/<str:pk>', views.update_group_task, name = 'update-grouptask'),
     path('personal-task/<str:pk>', views.create_personalTask, name = "create-pt"),
     path('group-task/<str:pk>', views.create_group_task, name = "create-gt"),
     path('update-pt/<str:pk>', views.update_personalTask, name = "update-personalTask"),
-    path('mark-task/<str:pk>/', views.mark_completed, name = "mark-completed"),
+    path('mark-task/<str:pk>/', views.mark_completed_personaltask, name = "mark-completed"),
+    path('mark-grouptask/<str:pk>/', views.mark_completed_grouptask, name = "mark-completed-gt"),
     path('search-projects/', views.search_projects, name = 'search-project'),
 
     #task-links
--- TaskManager/projects/views.py
+++ TaskManager/projects/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from requests import RequestException
 from .models import GroupProject, GroupTask, PersonalTask, PersonalProjects
 from .forms import GroupProjectForm, GroupTaskForm, TaskForm, PersonalProjectForm, PersonalTaskForm
 
@@ -28,7 +29,60 @@ def project_view(request):
     
     return render(request, 'projects/project_home.html', {'contrib':list_tasks})
 
-#get default project for showing
+#generic function/ get_default_context
+#generic function/ get defualt project
+class DefaultProject:
+
+    def __init__(self, task_model, project_model, pk, user_id) -> None:
+        filter_project = Q(created_by = user_id)
+        self.filter_one = Q(project = pk)
+        self.filter_two = Q(is_completed = False)
+        self.filter_three = Q(is_completed = True)
+
+        self.pending = task_model.objects.filter(self.filter_one & self.filter_two)
+        self.completed = task_model.objects.filter(self.filter_one & self.filter_three)
+        self.project = project_model.objects.get(id=pk)
+        self.context = {}
+    
+    #get all projects
+    def get_all_projects(self):
+        return self.project.objects.filter(self.filter_project)
+    #get project
+    def get_project(self):
+        return self.project
+    
+    #get pending task
+    def get_pending_task(self):
+        return self.pending
+    
+    #get completed task
+    def get_completed_task(self):
+        return self.completed
+
+    #get count of pending task
+    def get_count_pending(self):
+        return self.completed
+    
+    #get count of completed task
+    def get_count_completed(self):
+        return self.completed.count()
+    
+    # get count of pending task
+    def get_count_pending(self):
+        return self.pending.count()
+    
+    # get context
+    def get_context(self, project_type:str):
+
+        self.context['pending'] = self.get_pending_task
+        self.context['completed'] = self.get_completed_task
+        self.context['count_pending'] = self.get_count_pending
+        self.context['count_completed'] = self.get_count_completed
+        self.context[f'{project_type}_default'] = self.get_project
+        self.context['projects'] = self.get_all_projects
+
+        return self.context
+
 def get_default_context(pk, model_object, default_ctx):
 
         filter_one = Q(project = pk)
@@ -39,6 +93,7 @@ def get_default_context(pk, model_object, default_ctx):
         completed = model_object.objects.filter(filter_one & filter_three)
         
         context = {
+            
             'pending': pending, 
             'count_pending':pending.count(),
 
@@ -53,9 +108,10 @@ def get_default_context(pk, model_object, default_ctx):
 @login_required(login_url='login')
 def show_personal_projects(request, pk):
     project = PersonalProjects.objects.get(id=pk)
-    context = {}
+
     if request.user == project.created_by:
-        context = get_default_context(pk, PersonalTask,  PersonalProjects)
+        personal_project = DefaultProject(PersonalTask, PersonalProjects, pk, request.user.id)
+        context  = personal_project.get_context('personal')
         if request.method =="POST":
             if 'project_name' in request.POST:
                 create_personalProject(request, request.user)
@@ -65,17 +121,18 @@ def show_personal_projects(request, pk):
             pass
     else:
         messages.error(request, 'Requested anauthorized page')
+    print(context)
     return render(request, 'projects/project_page.html', context)
 
 #show all personal projects
 @login_required(login_url='login')
 def default_personalProject(request):
     All_projects  = PersonalProjects.objects.filter(created_by = request.user.id)
-    
+
+    personal_project = DefaultProject(PersonalTask, PersonalProjects, All_projects[0].id,request.user.id)
     context = {}
     if All_projects:
-        context = get_default_context(All_projects[0].id, PersonalTask, PersonalProjects)
-        context['projects'] = All_projects
+        context = personal_project.get_context('personal')
 
     return render(request, 'projects/project_page.html', context)
 
@@ -158,7 +215,6 @@ def create_group_task(request, pk):
             obj.save()
 
             colloborators = list(map(int, request.POST.getlist('assignedTo')))
-            print(colloborators)
             obj = GroupTask.objects.get(id = obj.id)
             obj.assignedTo.set(id for id in colloborators)
             obj.save()
@@ -174,20 +230,6 @@ def create_group_task(request, pk):
         messages.add_message(request, messages.INFO, "YOu are not authorized to create a task")
     return render(request, 'dashboard/index.html', get_default_context(pk, GroupTask, GroupProject))
 
-#edit tasks pt(personal_task)
-def update_personalTask(request, pk):
-    task = PersonalTask.objects.get(id = pk)
-
-    form  = PersonalTaskForm(instance=task)
-    if request.method == "POST":
-        form = PersonalTaskForm(request.POST, instance=task)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-
-    context = {'form':form}
-    return render(request, 'projects/pt_form.html', context)
-
 #delete personal Project
 def delete_personalProject(request, pk):
     project  = PersonalProjects.objects.get(id = pk)
@@ -198,48 +240,96 @@ def delete_personalProject(request, pk):
         project.delete()
     
     return render(request, 'projects/project_page.html', {'obj':project})
-    
-#delete personal Task
-@login_required(login_url='login')
-def delete_personalTask(request, pk):
-    task = PersonalTask.objects.get(id=pk)
+
+#generic function/deleting task
+def delete_task(request, task_model_name, project_model_name,  pk):
+    task = task_model_name.objects.get(id = pk)
+    project  = project_model_name.objects.get( id = task.project_id)
 
     if request.method == "POST":
         task.delete()
+
+        #reducing number of counts
+        project.count -=1
+        project.save()
+        return True
+    return False
+
+#delete personal Task
+@login_required(login_url='login')
+def delete_personalTask(request, pk):
+    delete_task(request, PersonalTask, PersonalProjects, pk)
     return render(request, 'projects/project_page.html')
 
-# mark tasks completed
-@login_required(login_url='login')
-def mark_completed(request, pk):
-    task = PersonalTask.objects.get(id = pk)
-    project = PersonalProjects.objects.get(id = task.project.id)
-
-    if request.method == "POST":
-        task.is_completed = True
-        task.save()
-
-        # return redirect('show-pp')
-
-    return render(request, 'projects/project_page.html', get_default_context(project.id, PersonalTask,  PersonalProjects))
-
+def delete_group_task(request, pk):
+    delete_task(request, GroupTask, GroupProject, pk)
+    return render(request, 'dashboard/index.html')
 
 #search project functionality 
 @login_required(login_url='login')
 
 def search_projects(request):
-    query = request.GET.get('query') if request.GET.get('query') is not None else ''
 
+    query = request.GET.get('query') if request.GET.get('query') is not None else ''
     try:
         context = {}
         project = PersonalProjects.objects.get(Q(project_name__icontains = query))
-
         if project.created_by == request.user:
-            context = get_default_context(project.id, PersonalProjects,  PersonalProjects)
-
+            personal_project  = DefaultProject(PersonalTask, PersonalProjects, project.id, request.user.id)
+            context = personal_project.get_context('personal')
         return render(request, 'projects/project_page.html', context)
 
     except Exception as e:
         return render(request, 'projects/project_page.html', {})
+
+#generic function/ updating task
+def update_task(request, model_name, form_name, pk):
+    task = model_name.objects.get(id=pk)
+    form = form_name(instance = task)
+
+    if request.method == "POST":
+        form = form_name(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            redirect(request.META['HTTP_REFERER'])
+    return form
+
+#edit tasks pt(personal_task)
+def update_personalTask(request, pk):
+    form = update_task(request, PersonalTask, PersonalTaskForm, pk)
+    context = {'form':form}
+    return render(request, 'projects/pt_form.html', context)
+
+#update - group task
+def update_group_task(request, pk):
+    update_task(request, GroupTask, GroupTaskForm, pk)
+
+    return render(request, 'dashboard/index.html')
+
+#generic function/mark_completed
+def mark_completed(request, task_model_name, project_model_name, pk):
+    task = task_model_name.objects.get(id = pk)
+    project = project_model_name.objects.get(id = task.project.id)
+
+    if request.method == "POST":
+        task.is_completed = True
+        task.save()
+    
+    return project
+
+#mark_completed personal task
+@login_required(login_url='login')
+def mark_completed_personaltask(request, pk):
+    project = mark_completed(request, PersonalTask, PersonalProjects, pk)
+    
+    return render(request, 'projects/project_page.html', get_default_context(project.id, PersonalTask,  PersonalProjects))
+
+#mark_completed group task
+@login_required(login_url='login')
+def mark_completed_grouptask(request, pk):
+    mark_completed(request, GroupTask, GroupProject, pk)
+    return redirect(request.META['HTTP_REFERER'])
+
     
 
 @login_required(login_url='login')
@@ -275,10 +365,6 @@ def start_task(request):
         return redirect('homepage')
     else:
         return render(request, 'Team/my_profile.html')
-
-@login_required(login_url='login')
-def update_task(request, pk):
-    pass
 
 #show team tasks
 @login_required(login_url='login')
